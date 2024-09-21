20efaed1312289a3a1c991e79cfb66cf7d546ba1
share-a-byte <write2vaniramesh@gmail.com>
2024-09-20 23:26:08 -0500

    fixed

--- src/components/ui/textarea.jsx
+++ src/components/ui/textarea.jsx
@@ -6,7 +6,7 @@ const Textarea = React.forwardRef(({ className, ...props }, ref) => {
   return (
     <textarea
       className={cn(
-        "flex min-h-[80px] w-full rounded-md border px-3 py-2 text-sm text-black focus-visible:outline-none focus-visible:ring-2  focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 border-gray-800 bg-white ring-offset-gray-950 placeholder:text-gray-500 focus-visible:ring-gray-300",
+        "flex min-h-[80px] w-full rounded-md border px-3 py-2 text-sm text-white focus-visible:outline-none focus-visible:ring-2  focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 border-gray-800 bg-gray-950 ring-offset-gray-950 placeholder:text-gray-500 focus-visible:ring-gray-300",
         className
       )}
       ref={ref}
