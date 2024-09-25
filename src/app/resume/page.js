b47ad41d8b3ecddff1d3241d2d7b262497b17455
share-a-byte <write2vaniramesh@gmail.com>
2024-09-25 11:07:00 -0500

    resume again

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -6,7 +6,7 @@ export default function ResumeOpener() {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
       <object
-        data="/files/Vani_Ramesh_Resume.pdf"
+        data="/files/VANI_RESUME_2027.pdf"
         type="application/pdf"
         width="100%"
         height="100%"
@@ -14,7 +14,7 @@ export default function ResumeOpener() {
         <p className="flex flex-row min-h-screen justify-center items-center">
           Unable to display PDF file.&nbsp;
           <span className="underline text-cyan-200">
-            <a href="/files/Vani_Ramesh_Resume.pdf">Download</a> instead.
+            <a href="/files/VANI_RESUME_2027.pdf">Download</a> instead.
           </span>
         </p>
       </object>
